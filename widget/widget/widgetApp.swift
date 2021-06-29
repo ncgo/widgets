b79e1b6cda21e58f3025ce379b6b7c296dcfc206
nadia.co <ngarciaorozco@outlook.com>
2021-06-28 20:00:31 -0600

    Initial MacOS setup

---
+++ widget/widget/widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetApp.swift
+//  widget
+//
+//  Created by Nadia Garcia on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
